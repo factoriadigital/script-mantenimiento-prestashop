0cdada77da282dbcd98f0c262c59cf5c185f23fb
Toni Cortés <info@tonicort.es>
2018-10-09 17:13:27 +0100

    Create prestashop16_maintenance.sh

---
+++ prestashop16_maintenance.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Show log messages
+VERBOSE=true
+
+# Vars
+ROOT_DIR="/home/" # Absolute path to this script
+CACHE_IMAGES_EXPIRATION=180
+
+# Function declarations
+function usage() 
+{
+    echo
+    echo "Usage: $0"
+    echo
+
+    exit 1
+}
+
+function clean_themes_cache()
+{
+    for theme in $1/themes/*;
+    do
+        if [ -d "$theme/cache" ]; then
+            find $theme/cache ! -name 'index.php' -type f -atime +$CACHE_IMAGES_EXPIRATION -exec rm -f {} \;
+            show_log "Cleaned theme cache: $theme"
+        fi        
+    done
+}
+
+function clean_cache()
+{
+    show_log "Starting the clean cache images process..."
+    find $1/cache/smarty/compile ! -name 'index.php' -type f -atime +$CACHE_IMAGES_EXPIRATION -exec rm -f {} \;
+    find $1/cache/smarty/cache ! -name 'index.php' -type f -atime +$CACHE_IMAGES_EXPIRATION -exec rm -f {} \;
+    find $1/cache/cachefs -type f -atime +$CACHE_IMAGES_EXPIRATION -exec rm -f {} \;
+    show_log "Cleaned smarty cache with access date greater than $CACHE_IMAGES_EXPIRATION days."
+    clean_themes_cache $1
+    show_log "Clean cache process finished."
+}
+
+function clean()
+{
+    show_log "-----------------------------------------------------------------------"
+    show_log "Prestashop installation found on: $1"    
+    clean_cache $1
+    show_log "-----------------------------------------------------------------------"
+}
+
+function show_log()
+{
+    if [ "$VERBOSE" = true ]; then
+        echo $1
+    fi    
+}
+
+# For multiple Prestashop installations
+for dir in *;
+do
+    # If Prestashop is found
+    if [ -f "$ROOT_DIR$dir/public_html/config/settings.inc.php" ]; then    
+        clean "$ROOT_DIR$dir/public_html"
+    fi
+done
+
+# For a single Prestashop installation
+
+# If Prestashop 1.6 is found
+#if [ -f "$ROOT_DIRconfig/settings.inc.php" ]; then    
+#    clean "$ROOT_DIR"
+#fi
